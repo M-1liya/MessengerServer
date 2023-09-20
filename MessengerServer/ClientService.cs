5d7159021d0fd76ca00527e2c685cf79d1eaf863
metel <metel@DESKTOP-BV43ST9>
2023-09-20 22:57:22 +0700

    Добавьте файлы проекта.

---
+++ MessengerServer/ClientService.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerServer
+{
+    internal partial class Program
+    {
+
+        static void ConnectClient(object? _client)
+        {
+            int? _IdClient = null;
+            TcpClient client;
+
+            if (_client == null || _client is not TcpClient) return;
+            client = (TcpClient)_client;
+
+            NetworkStream _stream = client.GetStream();       //Получение потока
+
+
+            //Отправка первого сообщения
+
+            string[] Drivers = Environment.GetLogicalDrives();
+            const string FirstMessage = "HEAR_ME?";
+            SendAnswer(_stream, FirstMessage);
+
+            Console.WriteLine("Server sent first message: \n" + FirstMessage);
+            //
+            string request = GetRequest(_stream);
+
+            if (request == null || request == "")
+            {
+                _stream.Close();
+                client.Close();
+                return;
+            }
+
+            request = GetRequest(_stream);
+
+            if (request == "LOG_IN")
+            {
+                string log_in_str = GetRequest(_stream);
+                LogIn(log_in_str, ref _IdClient);
+                //Результат входа
+            }
+            else if (request == "AUTH")
+            {
+                string auth_str = GetRequest(_stream);
+                Authorization(auth_str, ref _IdClient);
+                //Результат авторизации
+            }
+            else
+                SendAnswer(_stream, "Error: Expected 'LOG_IN' or 'AUTH'");
+
+
+
+            try
+            {
+                while (true)
+                {
+
+                    request = GetRequest(_stream); //Ожидание запроса
+
+                    string[] arrRequest = request.Split("::");
+                    if (arrRequest[0] == "SEND")
+                    {
+                        if (arrRequest.Length < 3)
+                        {
+                            SendAnswer(_stream, "WRONG_TEMPLATE");
+                            continue;
+                        }
+
+                        if (arrRequest.Length > 3)
+                            for (int i = 3; i < arrRequest.Length; i++)
+                                arrRequest[2] += arrRequest[i];
+
+                        SendMessageToClient(_IdClient.ToString(), arrRequest[1], arrRequest[2]);
+                    }
+                    else if (false) { }
+                    else
+                        SendAnswer(_stream, "INVALID_REQUEST");
+
+                }
+            }
+            catch { Console.WriteLine("Соединение с клиентом потеряно"); }
+        }
+
+
+
+        private static string LogIn(string log_in_str, ref int? id)
+        {
+            //Обращение к БД проверка наличия пользователя и ответ
+            return "SUCCESS";
+        }
+
+        private static string Authorization(string auth_str, ref int? id)
+        {
+            //Обращение к БД, проверка совпадений, результат
+            return "SUCCESS";
+        }
+
+
+        /// <param name="id_client">id отправителя</param>
+        /// <param name="id_addressee">id получателя сообщения</param>
+        /// <param name="message"></param>
+        /// <param name="_stream"></param>
+        private static void SendMessageToClient(string? id_client, string id_addressee, string message)
+        {
+            //Поиск получателя по БД, если есть такой, то ноходим его файл и записываем в него сообщение
+
+            string ME = id_client == null? "" : id_client;
+            string[] lines = File.ReadAllLines(id_addressee + ".txt");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] == $"FROM::{ME}:")
+                {
+                    for (int j = i; j < lines.Length; j++)
+                        if (lines[j + 1] == $"<{ME}>")
+                        {
+                            lines[j] = message;
+                            break;
+                        }
+                    break;
+                }
+                if (i + 1  == lines.Length)
+                {
+                    File.AppendAllText(id_addressee,
+                        $"FROM::{ME}:\n" +
+                        $"{message}\n" +
+                        $"\n" +
+                        $"<{ME}>");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ MessengerServer/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace MessengerServer
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            StartServer();
+        }
+
+
+
+        static void StartServer()
+        {
+            TcpListener _server = new TcpListener(IPAddress.Any, 2000); //port = 2000
+
+            _server.Start();
+            Console.WriteLine("Server started");
+
+            string request = "";
+
+            while (request != "STOP_WORK")
+            {
+                TcpClient _client = _server.AcceptTcpClient();      //Ожидание подключения клиента
+
+                Thread myThread1 = new Thread(new ParameterizedThreadStart(ConnectClient));
+            }
+        }
+
+        
+
+        /// <summary>
+        /// Отправляет ответ клиенту
+        /// </summary>
+        /// <param name="stream"> Поток через который идет общение с клиентом</param>
+        /// <param name="message"></param>
+        private static void SendAnswer(NetworkStream stream, string message)
+        {
+            var answer = Encoding.UTF8.GetBytes(message);
+            stream.Write(answer);
+            stream.Flush();
+        }
+
+        /// <summary>
+        /// Ожидание запроса от клиента
+        /// </summary>
+        /// <param name="stream">Поток через который идет общение с клиентом</param>
+        /// <returns>Возвращает строку с запросом клиента</returns>
+        private static string GetRequest(NetworkStream stream)
+        {
+            string request = "";
+            byte[] buffer = new byte[1024];
+
+            int lenght = stream.Read(buffer, 0, buffer.Length);
+            if (lenght > 0) request = Encoding.UTF8.GetString(buffer, 0, lenght);
+
+            return request;
+        }
+
+        /// <param name="path"></param>
+        /// <returns> Возвращает строку-ответ из имен файлов и директорий, находящихся по адресу пути</returns>
+        private static string CreateAnswer(string path)
+        {
+            string result = "";
+
+            string[] dir = Directory.GetFileSystemEntries(path);
+            foreach (string dirEntry in dir)
+            {
+                result += dirEntry + "\n";
+            }
+
+
+            return result != "" ? result : "Здесь пока что пусто";
+        }
+
+
+    }
+}
